56fb56f7fd8138f105cd19e72eff5d43db4ffdef
Himanshi Jangid <himanshijangid444@gmail.com>
2023-03-21 16:13:43 +0530

    fcgvg

---
+++ script.js
@@ -0,0 +1,6 @@
+const choices = document.querySelectorAll(".choices");
+const result = document.querySelector("#result");
+const restart = document.querySelector("#restart");
+const score = document.querySelector("#score");
+const modal = document.querySelector ("#modal");
+const scoreboard
