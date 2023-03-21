db6ce66adbfe0824218486361310d1d8b47078c0
Himanshi Jangid <himanshijangid444@gmail.com>
2023-03-21 16:18:12 +0530

    nbn

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ const result = document.querySelector("#result");
 const restart = document.querySelector("#restart");
 const score = document.querySelector("#score");
 const modal = document.querySelector ("#modal");
-const scoreboard
+const scoreboard ={
+    player : 0;
+    computer : 0 ;
+}
